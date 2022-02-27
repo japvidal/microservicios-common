20bc73c82acaeddfc1a58897fb336f1ed86efec4
japerez <japvidal@gmail.com>
2022-02-27 16:39:30 +0100

    BR_1.001
    First upload common microservice of futfem project

---
+++ src/main/java/com/microservicios/app/common/MicroserviciosCommonApplication.java
@@ -0,0 +1,8 @@
+package com.microservicios.app.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosCommonApplication {
+
+}
